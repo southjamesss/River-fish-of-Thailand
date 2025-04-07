0437596af4c997a815b796186379f605da8d8abe
Manatsanan Khongam <manatsanankhongam@192.168.0.103>
2025-04-07 20:05:50 +0700

    อัปเดต

--- frontend/src/components/AboutPage.jsx
+++ frontend/src/components/AboutPage.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-green-50">
-      <h1 className="text-4xl font-bold text-green-700">เกี่ยวกับเรา (About Page)</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-green-50">
+      <h1 className="text-4xl font-bold text-green-700">สนใจพัฒนาเว็บไซต์ภาษา React และ TailwindCSS</h1>
+      <br />
+      <h1 className="text-4xl font-bold text-green-700">manat1719@gmail.com</h1>
     </div>
   );
 };
